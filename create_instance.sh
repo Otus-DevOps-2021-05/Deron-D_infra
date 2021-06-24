c9320fd974d6c9458e62842de0ef180bf3481671
Dmitriy Pnev <dmitriypnev@gmail.com>
2021-06-24 15:41:52 +0300

    Commit #3 cloud-testapp

--- create_instance.sh
+++ create_instance.sh
@@ -7,8 +7,7 @@ testapp_IP=$(yc compute instance create \
  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
  --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
  --metadata serial-port-enable=1 \
- --ssh-key ~/.ssh/appuser.pub | grep 'address'| tail -1 | awk '{print $2}') &&
-echo "'''" >> README.md
+ --ssh-key ~/.ssh/appuser.pub | grep 'address'| tail -1 | awk '{print $2}')
+
 echo "testapp_IP = $testapp_IP" >> README.md
 echo "testapp_port = 9292" >> README.md
-echo "'''" >> README.md
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
