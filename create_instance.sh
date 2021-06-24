b7a313d09588c36fe0ce577929a782e250c7ce5e
Dmitriy Pnev <dmitriypnev@gmail.com>
2021-06-24 14:46:32 +0300

    Commit #1 cloud-testapp

---
+++ create_instance.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+testapp_IP=$(yc compute instance create \
+ --name reddit-app \
+ --hostname reddit-app \
+ --memory=4 \
+ --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
+ --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
+ --metadata serial-port-enable=1 \
+ --ssh-key ~/.ssh/appuser.pub | grep 'address'| tail -1 | awk '{print $2}') &&
+echo "'''" >> README.md
+echo "testapp_IP = $testapp_IP" >> README.md
+echo "testapp_port = 9292" >> README.md
+echo "'''" >> README.md
