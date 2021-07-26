aeef8b40026f0a7fc0b9992209b3108edfaf77b7
Dmitriy Pnev <dmitriypnev@gmail.com>
2021-07-26 18:22:29 +0300

    Divide configuration by purpose of creation 2 VMs

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt update
-sleep 1m
+sleep 30s
 apt install -y ruby-full ruby-bundler build-essential
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,9 +13,9 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable image_id {
-  description = "Disk image"
-}
+# variable image_id {
+#   description = "Disk image"
+# }
 # variable subnet_id {
 #   description = "Subnet"
 # }
@@ -32,4 +32,12 @@ variable region_id {
 variable count_of_instances {
   description = "Count of instances"
   default     = 1
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+variable db_disk_image {
+description = "Disk image for reddit db"
+default = "reddit-db-base"
 }
